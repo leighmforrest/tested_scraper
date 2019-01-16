a0019a89b1f52ec5784e7a326cb5616bc55c7509
Leigh Forrest <leighmforrest@gmail.com>
2019-01-15 20:26:54 -0500

    mostly tested web scraper

---
+++ scraper.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def get_soup(self):
+        self.html = requests.get(self.site).text
+        self.soup = BeautifulSoup(self.html, 'html.parser')
+        return self.soup
+
+    def get_text(self, element):
+        """Strip out text from ResultSet"""
+        for wrapper in element:
+            return wrapper.text.strip().lower()
+
+
+class Chart(Scraper):
+    def get_date(self):
+        """Get a date string. Returned string does not conform to Python's formatting."""
+        self.get_soup()
+        date = self.soup.findAll("button", {"class": "chart-detail-header__date-selector-button"})
+        date = self.get_text(date)
+        return date
+
+    def get_next_weeks_link(self):
+        """Get the link for the following week."""
+        self.get_soup()
+        next_week = None
+        dropdowns = self.soup.findAll("li", {"class": "dropdown__date-selector-option"})
+        for links in dropdowns:
+            for link in links.find_all('a'):
+                if link.find('span', {"class": "fa-chevron-right"}):
+                    print(link)
+                    next_week = 'https://www.billboard.com' + link['href']
+        return next_week
+
+    def get_chart_topper(self):
+        """Get the number one song."""
+        title = self.soup.findAll("div", {"class": "chart-number-one__title"})
+        title = self.get_text(title)
+        artist = self.soup.findAll("div", {"class": "chart-number-one__artist"})
+        artist = self.get_text(artist)
+        return {'rank': 1, 'artist': artist, 'title': title}
+
+    def get_songs(self):
+        """Get all 100 songs in the chart."""
+        rankings = [self.get_chart_topper()]
+
+        songs = self.soup.findAll("div", {"class": "chart-list-item"})
+        for song in songs:
+            rank = artist = int(song.attrs['data-rank'])
+            artist = song.attrs['data-artist'].strip().lower()
+            title = song.attrs['data-title'].strip().lower()
+            ranking = {
+                'rank': rank,
+                'artist': artist,
+                'title': title
+            }
+            rankings.append(ranking)
+
+        return rankings
---
+++ tests/test_scraper.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+import pytest
+import requests
+from unittest import mock
+
+
+from scraper.scraper import Scraper
+
+
+def test_existence(scraper):
+    assert scraper is not None
+
+
+def test_has_site_url(scraper):
+    assert scraper.site == 'https://www.billboard.com/'
+
+
+def test_get_soup(scraper, webpage):
+    with mock.patch('requests.get') as m:
+        m.return_value.text = webpage
+        soup = scraper.get_soup()
+        assert soup.find('a') is not None
+
+
+def test_chart_existence(chart):
+    assert chart is not None
+
+
+def test_chart_has_site_url(chart):
+    assert chart.site == 'https://www.billboard.com/'
+
+
+def test_chart_get_soup(chart, webpage):
+    with mock.patch('requests.get') as m:
+        m.return_value.text = webpage
+        soup = chart.get_soup()
+        assert soup.find('a') is not None
+
+
+def test_chart_date(chart, webpage):
+    """Billboard has the date as 'January 19, 2019' '%B %-d, %Y'"""
+    with mock.patch('requests.get') as m:
+        m.return_value.text = webpage
+        date = chart.get_date()
+        assert date == 'january 19, 2019'
+
+
+def test_current_weeks_chart_has_no_next_week(chart, webpage):
+    with mock.patch('requests.get') as m:
+        m.return_value.text = webpage
+        next_week = chart.get_next_weeks_link()
+        assert next_week is None
+
+
+def test_get_chart_topper(chart, webpage):
+    with mock.patch('requests.get') as m:
+        m.return_value.text = webpage
+        topper = chart.get_chart_topper()
+        assert 'post malone' in topper['artist']
+        assert 'sunflower (spider-man: into the spider-verse)' == topper['title']
+        assert 1 == topper['rank']
+
+
+def test_previous_weeks_chart_has_next_week(chart, previous_webpage):
+    with mock.patch('requests.get') as m:
+        m.return_value.text = previous_webpage
+        next_week = chart.get_next_weeks_link()
+        assert next_week == 'https://www.billboard.com/charts/hot-100/2019-01-12'
+
+
+def test_get_previous_chart_topper(chart, previous_webpage):
+    with mock.patch('requests.get') as m:
+        m.return_value.text = previous_webpage
+        topper = chart.get_chart_topper()
+        assert 'ariana grande' in topper['artist']
+        assert 'thank u, next' == topper['title']
+        assert 1 == topper['rank']
+
+
+def test_get_chart_songs_is_100(chart, webpage):
+    with mock.patch('requests.get') as m:
+        m.return_value.text = webpage
+        songs = chart.get_songs()
+        assert len(songs) == 100
+
+
+def test_get_chart_songs_correct_format(chart, webpage):
+    with mock.patch('requests.get') as m:
+        m.return_value.text = webpage
+        songs = chart.get_songs()
+        for song in songs:
+            assert len(song['title']) > 0
+            assert len(song['artist']) > 0
+            assert song['rank'] > 0 and song['rank'] <= 100
